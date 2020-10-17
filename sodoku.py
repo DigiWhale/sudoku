302e778be0c74125ff7925e836979c1acf917759
digiwhale <digiwhale.humphrey@gmail.com>
2020-10-17 11:48:30 -0400

    updated new functions

--- sodoku.py
+++ sodoku.py
@@ -18,24 +18,40 @@ next meeting: saturday at 11AM CST
 #     - identify which numbers still need to be added
 #     - return list of [numbers_to_be_added]
 #     - return list of [numbers_already_there]
-
-def insert_a_value_into_a_square(row, column, value_to_insert):
+#Andrew
+def insert_a_value_into_a_square(self, row, column, value_to_insert):
     board[array_index][index_in_array] = value_to_insert #assigns a position on the board
     return board[array_index] #returns an array of the row that has been modified
 
+#Andrew
+def check_box(self, box, number_to_find) 
+    #box is a tupple of the start position of the upper left corner of the individual boxes 
+    #box_1 = (0,0) box_2 = (0,3) box_3 = (0,6)
+    #box_4 = (3,0) box_5 = (3,3) box_6 = (3,6) 
+    #box_7 = (6,0) box_8 = (6,3) box_9 = (6,6)
+    for i in range(3): #iterate over first 3 rows
+        for j in range(3): #iterate over the first 3 columns
+            if board[i+box[0]][j+box[1]] == n: #checks if number_to_find exists in a box
+                return True # if it finds the number it returns true
+            else:
+                return False #else it returns false to indicate number is not in the box
 
-def find_empty_cell(board_array):
-    for row in range(9):
-        for column in range(9):
-            if board[row][column] == 0:
-                return (row, column)
-
-def check_valid_placement(row=0, column=0, number_to_check=0): #rows and columns are indexed from the top left, Row 0 is on top, row 9 is on bottom
+#Andrew
+def find_empty_cell(self, board_array):
+    for row in range(9): #iterate through the row
+        for column in range(9): #iterate through the column
+            if board[row][column] == 0: #check for a 0
+                return (row, column) #returns the row and column of the 0
+                
+#Andrew
+def check_valid_placement(self, row=0, column=0, number_to_check=0): #rows and columns are indexed from the top left, Row 0 is on top, row 9 is on bottom
     #columns start on the left at column 0 and end on the right at column 9
     #check row and column
     for i in range(9):
-        if board[row][i] == number_to_check or board[i][column] == number_to_check:
-            return False
+        if board[row][i] == number_to_check or board[i][column] == number_to_check: #this checks if the number_to_check is the only number copy in the row and column
+            return False #returns false if it finds a duplicate in that row or colunm
+        else:
+            return True #returns true if the number is unique to the row and column
 
 # Augie
 # # def generate_board_array(self, array)
@@ -69,8 +85,15 @@ Standard variables-
 
     board_array = [[4, 8, 3, 9, 2, 1, 6, 5, 7],[9, 6, 7, 3, 4, 5, 8, 2, 1],[2, 5, 1, 8, 7, 6, 4, 9, 3],[5, 4, 8, 1, 3, 2, 9, 7, 6],[7, 2, 9, 5, 6, 4, 1, 3, 8],[1, 3, 6, 7, 9, 8, 2, 4, 5],[3, 7, 2, 6, 8, 9, 5, 1, 4],[8, 1, 4, 2, 5, 3, 7, 6, 9],[6, 9, 5, 4, 1, 7, 3, 8, 2]]
 
-
-
+    box_1= (0,0)
+    box_2= (0,3)
+    box_3= (0,6)
+    box_4= (3,0)
+    box_5= (3,3)
+    box_6= (3,6)
+    box_7= (6,0)
+    box_8= (6,3)
+    box_8= (6,6)
 
 
 #boggle solution
